c6857a030fee843519684ee7b5188df8b0ecf7e2
jerri323 <143474076+jerri323@users.noreply.github.com>
2023-10-08 12:28:10 +0700

    mengubah nama router jadi routes

--- controller.js
+++ controller.js
@@ -5,15 +5,14 @@ var connection = require('./koneksi');
 
 exports.index = function(req, res){
     response.ok("Aplikasi REST API ku berjalan!!",res)
-
 };
 
-    // menampilkan semua data mahasiswa
+ // menampilkan semua data mahasiswa
     exports.tampilsemuamahasiswa = function(req,res){
-        connection.query('SELECT * FROM mahasiswa', function(error, rows, failed){
+        connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds){
             if(error){
                 connection.log(error);
-            } else {
+            }else {
                 response.ok(rows, res)
             }
         });
